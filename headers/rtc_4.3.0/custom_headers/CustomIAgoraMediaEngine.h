8848cc92af5973d1efb3fa2dc635007554163896
Arthas <15215604969@163.com>
2023-10-09 11:44:27 +0800

    feat: add 4.3.0 head file (#7)
    Co-authored-by: 夏羊群 <xiayangqun@agora.io>

---
+++ headers/rtc_4.3.0/custom_headers/CustomIAgoraMediaEngine.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include "AgoraBase.h"
+#include "IAgoraMediaEngine.h"
+
+namespace agora {
+namespace media {
+namespace ext {
+
+class IMediaEngine {
+  // ----------------------------- 👇🏻new API👇🏻 -----------------------------
+
+  // add for registerAudioFrameObserver
+  // virtual int registerAudioFrameObserver(IAudioFrameObserver* observer) = 0;
+  virtual int unregisterAudioFrameObserver(IAudioFrameObserver *observer) = 0;
+
+  // add for registerVideoFrameObserver
+  // virtual int registerVideoFrameObserver(IVideoFrameObserver* observer) = 0;
+  virtual int unregisterVideoFrameObserver(IVideoFrameObserver *observer) = 0;
+
+  // add for registerVideoEncodedFrameObserver
+  // virtual int registerVideoEncodedFrameObserver(IVideoEncodedFrameObserver* observer) = 0;
+  virtual int
+  unregisterVideoEncodedFrameObserver(IVideoEncodedFrameObserver *observer) = 0;
+
+  // ----------------------------- 👆🏻new API👆🏻 -----------------------------
+};
+
+class IMediaEngineS {
+  // ----------------------------- 👇🏻new API👇🏻 -----------------------------
+
+  // add for registerAudioFrameObserver
+  // virtual int registerAudioFrameObserver(IAudioFrameObserver* observer) = 0;
+  virtual int unregisterAudioFrameObserver(IAudioFrameObserverBase *observer) = 0;
+
+  // add for registerVideoFrameObserver
+  // virtual int registerVideoFrameObserver(IVideoFrameObserver* observer) = 0;
+  virtual int unregisterVideoFrameObserver(IVideoFrameObserverS *observer) = 0;
+
+  // add for registerVideoEncodedFrameObserver
+  // virtual int registerVideoEncodedFrameObserver(IVideoEncodedFrameObserver* observer) = 0;
+  virtual int
+  unregisterVideoEncodedFrameObserver(IVideoEncodedFrameObserverS *observer) = 0;
+
+  // ----------------------------- 👆🏻new API👆🏻 -----------------------------
+};
+
+} // namespace ext
+} // namespace media
+} // namespace agora
