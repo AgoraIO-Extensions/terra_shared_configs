67fab1ad3867d1126d28c1394e40380cc7823a94
guoxianzhe <53285945+guoxianzhe@users.noreply.github.com>
2023-10-31 21:19:16 +0800

    chore: change to src/index.ts (#12)

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,6 @@ module.exports = {
   coverageDirectory: 'coverage',
   preset: 'ts-jest',
   testEnvironment: 'node',
+  transformIgnorePatterns: ['node_modules/(?!(@agoraio-extensions)/)'],
   testRegex: 'src/__tests__/.*\\.(test|spec)\\.[jt]sx?$',
 };
