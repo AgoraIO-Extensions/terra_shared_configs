ea5b0574dace3a434ad87dbeac2d43a5b62ef636
Littlegnal <8847263+littleGnAl@users.noreply.github.com>
2023-11-28 18:43:37 +0800

    feat: Add PointerMarkerParser (#19)
    Add PointerMarkerParser to explicitly mark the pointer type
    1. Pointer array
    2. Just a pointer

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,6 @@ export * from './parsers/order_node_parser';
 export * from './parsers/pointer_to_array_parser';
 export * from './parsers/remove_node_parser';
 export * from './parsers/update_simple_type_parser';
+export * from './parsers/pointer_marker_parser';
 export * from './renderers/mustache_renderer';
 export * from './renderers/iris_doc_renderer';
