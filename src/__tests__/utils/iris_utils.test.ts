3db789b484413b2c7deb4fe95bf728161d1ff393
littleGnAl <tearjeaker@hotmail.com>
2023-12-22 22:55:25 +0800

    Refactor irisApiType

---
+++ src/__tests__/utils/iris_utils.test.ts
@@ -0,0 +1,41 @@
+import path from 'path';
+import { irisApiType } from '../../index';
+import { Clazz, MemberFunction, Variable } from '@agoraio-extensions/cxx-parser';
+
+describe('irisApiType', () => {
+    it('return full api type with hash code', () => {
+        let myClass = { name: 'MyClass' } as Clazz;
+        let mf = {
+            name: 'MyFunc',
+            parameters: [
+                {
+                    type: {
+                        source: 'const char *'
+                    }
+                } as Variable
+            ]
+        } as MemberFunction;
+
+        let apiType = irisApiType(myClass, mf);
+
+        expect(apiType).toBe('MyClass_MyFunc_3766a1b9');
+    });
+
+    it('return hash code only', () => {
+        let myClass = { name: 'MyClass' } as Clazz;
+        let mf = {
+            name: 'MyFunc',
+            parameters: [
+                {
+                    type: {
+                        source: 'const char *'
+                    }
+                } as Variable
+            ]
+        } as MemberFunction;
+
+        let apiType = irisApiType(myClass, mf, true);
+
+        expect(apiType).toBe('3766a1b9');
+    });
+});
