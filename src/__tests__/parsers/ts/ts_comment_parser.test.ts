6416baec9420032d6816f535847c7ad9aeab77db
guoxianzhe <53285945+guoxianzhe@users.noreply.github.com>
2024-01-08 19:52:52 +0900

    TsCommentParser (#36)
    * original comment: \n0x0008: Critical errors that may lead to program
    termination.\n
     * after format: 
    <img width="646" alt="image"
    src="https://github.com/AgoraIO-Extensions/terra_shared_configs/assets/53285945/84023dc1-88ee-4488-bc85-7ced634c488f">

---
+++ src/__tests__/parsers/ts/ts_comment_parser.test.ts
@@ -0,0 +1,79 @@
+import {
+  CXXFile,
+  genParseResultFromJson,
+} from '@agoraio-extensions/cxx-parser';
+import { TerraContext } from '@agoraio-extensions/terra-core';
+
+import { TsCommentParser } from '../../..';
+
+describe('TsCommentParser', () => {
+  it('parser comment', () => {
+    let json = `
+[
+    {
+        "file_path": "/my/path/IAgoraRtcEngine.h",
+        "nodes": [
+            {
+                "__TYPE": "Clazz",
+                "file_path": "/my/path/IAgoraRtcEngine.h",
+                "name": "Test",
+                "namespaces": [
+                    "agora",
+                    "rtc"
+                ],
+                "comment": "Clazz.",
+                "parent_name": "rtc"
+            },
+            {
+                "__TYPE": "Enumz",
+                "enum_constants": [
+                    {
+                        "__TYPE": "EnumConstant",
+                        "comment": "North America.",
+                        "name": "AUDIO_DEVICE1",
+                        "source": "",
+                        "value": ""
+                    },
+                    {
+                      "__TYPE": "EnumConstant",
+                      "comment": "North America.",
+                      "name": "AUDIO_DEVICE2",
+                      "source": "12",
+                      "value": "12"
+                    },
+                    {
+                      "__TYPE": "EnumConstant",
+                      "comment": "North America.",
+                      "name": "AUDIO_DEVICE3",
+                      "source": "",
+                      "value": ""
+                    }
+                ],
+                "file_path": "/my/path/IAgoraRtcEngine.h",
+                "comment": "Enumz",
+                "name": "MEDIA_DEVICE_TYPE",
+                "namespaces": [
+                    "agora",
+                    "rtc"
+                ],
+                "parent_name": "Test"
+            }
+        ]
+    }
+]
+`;
+    let preParseResult = genParseResultFromJson(json);
+
+    const result = TsCommentParser(new TerraContext(), {}, preParseResult)
+      ?.nodes[0] as CXXFile;
+    expect(result?.nodes[0].comment).toEqual(`/**
+Clazz.
+*/`);
+    expect(result?.nodes[1].comment).toEqual(`/**
+Enumz
+*/`);
+    expect(result?.nodes[1].asEnumz().enum_constants[0].comment).toEqual(`/**
+North America.
+*/`);
+  });
+});
