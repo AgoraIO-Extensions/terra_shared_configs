83cab4677ce959ed0688f30ffdf3d9eb68aec114
littleGnAl <tearjeaker@hotmail.com>
2024-04-02 18:59:56 +0800

    Add diff_headers.yml action

---
+++ scripts/diff.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -e
+set -x
+
+BASE=$1
+TARGET=$2
+
+BASE_PATH=headers/${BASE}/include
+TARGET_PATH=headers/${TARGET}/include
+
+RESULT=$(diff -u -b -r ${BASE_PATH} ${TARGET_PATH})
+
+SUMMARY="\`\`\`diff\n""${RESULT}""\n\`\`\`"
+
+# Output the github action summary.
+echo '${SUMMARY}' >> $GITHUB_STEP_SUMMARY
