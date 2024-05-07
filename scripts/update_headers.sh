303f3b3732342a76105e717e7929e295c60551a0
guoxianzhe <53285945+guoxianzhe@users.noreply.github.com>
2024-05-07 14:26:34 +0800

    feat: support specific sdk headers version (#84)

--- scripts/update_headers.sh
+++ scripts/update_headers.sh
@@ -4,55 +4,65 @@ set -x
 MY_PATH=$(realpath $(dirname "$0"))
 PROJECT_ROOT=$(realpath ${MY_PATH}/..)
 
-# 检查是否提供了url
-if [ "$#" -ne 1 ]; then
+# Check if url is provided
+if [ "$#" -lt 1 ]; then
     echo "usage: \$0<url>"
     exit 1
 fi
 
-# 将参数分配给变量
+# Assign arguments to variables
 url=$1
-type=${url#*Preview_}; type=${type%%_*}
-version=${url#*${type}_}; version=${version%_*headers.zip}
+if [[ $url == *"Preview_"* ]]; then
+    type=${url#*Preview_}; type=${type%%_*}
+    version=${url#*${type}_}; version=${version%_*headers.zip}
+elif [[ $url == *"Agora_Native_SDK_"* ]]; then
+    type="rtc"
+    version=${url#*Windows_rel.v}; version=${version%%_*}
+fi
+
+# Check if second argument is provided, if so, use it as version
+if [ "$#" -ge 2 ]; then
+    version=$2
+fi
 latest_version="${version%.*}"
 
-# 构造目标目录路径
+# Construct the target directory path
 destination=${PROJECT_ROOT}"/headers/${type}_${version}"
 
-# 如果目标目录不存在，则复制最相似的一个文件夹并重命名
+# If the target directory does not exist, copy the most similar folder and rename it
 if [ ! -d "$destination" ]; then
-    # 找到最后一个文件夹
-    last_folder=$(ls -d ${PROJECT_ROOT}/headers/${type}_${latest_version}* | sort | tail -n 1)
-    # 复制最后一个文件夹并重命名
+    # Find the last folder
+    last_folder=$(ls -d ${PROJECT_ROOT}/headers/${type}_${latest_version}* | sort -V | tail -n 1)
+    # Copy the last folder and rename it
     echo "Copying $last_folder to $destination"
     cp -r "$last_folder" "$destination"
 else
-    # 如果目标目录已经存在，输出一条消息
+    # If the target directory already exists, output a message
     echo "The directory $destination already exists. no need to copy."
 fi
 
-# 删除include目录
+# Remove the include directory
 rm -rf "$destination/include"
 
-# 使用临时文件名下载文件
+# Download the file with a temporary filename
 temp_file="${destination}/temp_file.zip"
 curl -L -o "$temp_file" "$url"
 
-# 解压文件
+# Unzip the file
 unzip -o "$temp_file" -d "$destination" > /dev/null
 
-# 删除临时文件
+# Remove the temporary file
 rm "$temp_file"
 
-# 确保目标目录存在
+# Ensure the target directory exists
 mkdir -p "$destination"
 
-# 确保成功下载了nativeSDK
+# Ensure the nativeSDK was successfully downloaded
 nativeSDK="$destination/Agora_Native_SDK_for_Windows_FULL"
 mkdir -p $nativeSDK
 
-# 将文件移动到目标位置
+# Move the file to the target location
 mv "$nativeSDK/sdk/high_level_api/include" "$destination"
 
-# 删除解压后的文件
+# Remove the unzipped file
 rm -rf "$nativeSDK"
