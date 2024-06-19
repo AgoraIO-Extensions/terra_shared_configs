095275d81a63775fe20ad9aaaad45844ab81849e
sda-rob <149643938+sda-rob@users.noreply.github.com>
2024-06-19 12:03:27 +0800

    [AUTO] Update native headers (#102)
    native headers source:
    
    https://download.agora.io/sdk/release/AgoraRtcEngine_windows_Preview_rtc_4.2.6.14_headers.zip
    
    Co-authored-by: guoxianzhe <guoxianzhe@users.noreply.github.com>

---
+++ headers/rtc_4.2.6.14/custom_headers/CustomIAgoraMediaEngine.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "AgoraBase.h"
+#include "IAgoraMediaEngine.h"
+
+namespace agora {
+namespace media {
+namespace ext {
+
+class IMediaEngine {
+  // ----------------------------- 👇🏻new API👇🏻 -----------------------------
+
+  // add for registerAudioFrameObserver
+  // virtual int registerAudioFrameObserver(IAudioFrameObserver* observer) = 0;
+  virtual int unregisterAudioFrameObserver(IAudioFrameObserver *observer) = 0;
+
+  // add for registerVideoFrameObserver
+  // virtual int registerVideoFrameObserver(IVideoFrameObserver* observer) = 0;
+  virtual int unregisterVideoFrameObserver(IVideoFrameObserver *observer) = 0;
+
+  // add for registerVideoEncodedFrameObserver
+  // virtual int registerVideoEncodedFrameObserver(IVideoEncodedFrameObserver* observer) = 0;
+  virtual int
+  unregisterVideoEncodedFrameObserver(IVideoEncodedFrameObserver *observer) = 0;
+
+  // add for registerFaceInfoObserver
+  // virtual int registerFaceInfoObserver(IFaceInfoObserver* observer) = 0;
+  virtual int unregisterFaceInfoObserver(IFaceInfoObserver* observer) = 0;
+
+  // ----------------------------- 👆🏻new API👆🏻 -----------------------------
+};
+
+} // namespace ext
+} // namespace media
+} // namespace agora
