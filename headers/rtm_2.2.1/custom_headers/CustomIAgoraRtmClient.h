5af4287d542b8a2812208736d429791b1127e681
Littlegnal <8847263+littleGnAl@users.noreply.github.com>
2024-08-15 11:02:57 +0800

    Adapt rtm 2.2.1 (#127)
    Some changes when adapting the RTM 2.2.1

--- headers/rtm_2.2.1/custom_headers/CustomIAgoraRtmClient.h
+++ headers/rtm_2.2.1/custom_headers/CustomIAgoraRtmClient.h
@@ -13,10 +13,11 @@ namespace ext {
 
 class IRtmClient {
   // ----------------------------- 👇🏻new API👇🏻 -----------------------------
-
-  // IRtmClient
-  //virtual int publish(const char* channelName, const char* message, const size_t length, const PublishOptions& option, uint64_t& requestId) = 0;
-  virtual int publishWithBuffer(const char* channelName, const unsigned char* message, const size_t length, const PublishOptions& option, uint64_t& requestId) = 0;
+  /**
+   * @iris_api_id: RtmClient_publish_2d36e93
+   * @source: virtual void publish(const char* channelName, const char* message, const size_t length, const PublishOptions& option, uint64_t& requestId) = 0;
+   */
+  virtual void publishBinaryMessage(const char* channelName, const unsigned char* message, const size_t length, const PublishOptions& option, uint64_t& requestId) = 0;
 
   // ----------------------------- 👆🏻new API👆🏻 -----------------------------
 };
--- headers/rtm_2.2.1/custom_headers/CustomIAgoraStreamChannel.h
+++ headers/rtm_2.2.1/custom_headers/CustomIAgoraStreamChannel.h
@@ -9,10 +9,11 @@ namespace ext {
 
 class IStreamChannel {
   // ----------------------------- 👇🏻new API👇🏻 -----------------------------
-
-  // IStreamChannel
-  //virtual int publishTopicMessage(const char* topic, const char* message, size_t length, const PublishOptions& option) = 0;
-  virtual int publishTopicMessageWithBuffer(const char* topic, const unsigned char* message, size_t length, const PublishOptions& option) = 0;
+  /**
+   * @iris_api_id: StreamChannel_publishTopicMessage_a31773e
+   * @source: virtual void publish(const char* channelName, const char* message, const size_t length, const PublishOptions& option, uint64_t& requestId) = 0;
+   */
+  virtual void publishTopicBinaryMessage(const char* topic, const unsigned char* message, size_t length, const TopicMessageOptions& option, uint64_t& requestId) = 0;
 
   // ----------------------------- 👆🏻new API👆🏻 -----------------------------
 };
