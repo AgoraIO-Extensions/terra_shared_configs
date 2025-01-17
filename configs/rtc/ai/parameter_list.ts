9eabf699525d420dfa7d2e51ab9d85eef707d404
guoxianzhe <guoxianzhe@users.noreply.github.com>
2025-01-17 10:46:42 +0000

    [AUTO] Fetch AI JSON with https://download.agora.io/sdk/release/diff_4.5_differences_test.json

--- configs/rtc/ai/parameter_list.ts
+++ configs/rtc/ai/parameter_list.ts
@@ -1,1 +1,20 @@
-module.exports = {};
+module.exports = {
+  'IRtcEngine:getCapability.deviceIdUTF8@type': {
+    parent_class: 'IRtcEngine',
+    parent_name: 'getCapability',
+    is_output: false,
+    is_array: false,
+  },
+  'IRtcEngine:getCapability.deviceCapabilityNumber@type': {
+    parent_class: 'IRtcEngine',
+    parent_name: 'getCapability',
+    is_output: false,
+    is_array: false,
+  },
+  'IRtcEngine:getCapability.capability@type': {
+    parent_class: 'IRtcEngine',
+    parent_name: 'getCapability',
+    is_output: true,
+    is_array: false,
+  },
+};
